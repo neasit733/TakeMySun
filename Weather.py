23cc54d86dbbfd7497dbb9bd11b5016bc6bb063b
Nikki <49448031+neasit733@users.noreply.github.com>
2019-04-14 13:54:19 +0300

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,20 +1,20 @@
 from tkinter import *
-from tkinter import messagebox as mb #блять, потому что так легче писать
+from tkinter import messagebox as mb
 import time
 import locale
 import pyowm
 
 window = Tk()
-window.title("Weather")
+window.title("TsikaWeather")
 weight = window.winfo_screenwidth()//2 - 300
 height = window.winfo_screenheight()//2 - 300
 window.geometry('335x500+{}+{}'.format(weight, height))
 
+owm = pyowm.OWM(API_key="f61564d6483322c50316ab46ed520830", language='ru')
+
 locale.setlocale(locale.LC_ALL, "Russian_Russia.1251")
 date = "%x"
 t = time.strftime(date)
-
-owm = pyowm.OWM(API_key="f61564d6483322c50316ab46ed520830", language='ru')
 
 def weather():
     s = button_entry.get()
@@ -35,9 +35,9 @@ button_entry = Entry(width=25)
 button_get_weather = Button(text='Узнать погоду!', command=weather)
 
 string_date.place(x=0, y=0)
-label_request.place(x=100, y=100)
-button_entry.place(x=100, y=120)
-button_get_weather.place(x=128, y=145)
+label_request.place(x=100, y=120)
+button_entry.place(x=100, y=148)
+button_get_weather.place(x=128, y=165)
 
-window.resizable(False, False) #мрази, не трогайте мое окно
+window.resizable(False, False)
 window.mainloop()
